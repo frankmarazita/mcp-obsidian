dc4006ad1b94a3ec62b9b9bdb64304513236d3e3
Frank Marazita <fmarazita1@gmail.com>
2026-04-19 15:44:23 +1000

    fix runtime issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ -z "$PORT" ]; then
   PORT=3000
 fi
 
-mcp-proxy --pass-environment --sse-port=$PORT --sse-host 0.0.0.0 uv run mcp-obsidian
+mcp-proxy --pass-environment --sse-port=$PORT --sse-host 0.0.0.0 /mcp-obsidian/.venv/bin/mcp-obsidian
